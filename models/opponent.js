b1d9219a7e2b625bb2e5955d694dd7fbe5e22001
Tyler Mathena <tgmathena@crimson.ua.edu>
2019-05-29 12:11:20 -0400

    Fixed issues with models

--- models/opponent.js
+++ models/opponent.js
@@ -18,7 +18,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false
         },
         image: {
-            type: DataTypes.string,
+            type: DataTypes.STRING,
             allowNull: false
         },
         description: {
