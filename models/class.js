8968baf240c6c24dc2e6109cce642910e8a9117d
Tyler Mathena <tgmathena@crimson.ua.edu>
2019-05-29 10:42:23 -0400

    initial modeling for database

---
+++ models/class.js
@@ -0,0 +1,30 @@
+module.exports = function (sequelize, DataTypes) {
+
+    var Class = sequelize.define("Post", {
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        hp: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        ap: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        dp: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.string,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    });
+    return Class;
+};
