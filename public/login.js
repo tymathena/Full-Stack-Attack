93c3abbc147758477ca201548372dffd74876489
Abusch419 <abusch419@gmail.com>
2019-06-03 18:04:22 -0400

    adds hover animation to character card

--- public/login.js
+++ public/login.js
@@ -3,8 +3,8 @@ let currentUser;
 function renderLogIn() {
     $(".text-center").empty();
     $(".login").empty()
-    const logInForm = 
-    `<form class="row login">
+    const logInForm =
+        `<form class="row login">
         <div class="form-group col-12">
             <label for="email">Email address</label>
             <input type="text" class="form-control" id="email" placeholder="Enter User Name">
@@ -47,9 +47,9 @@ function tryLogInUser() {
                 name: $(`#email`).val(),
                 password: $(`#password`).val(),
             }
-            
+
             // add the user to the database
-            $.post("/api/user", newUser).then(function(newData) {
+            $.post("/api/user", newUser).then(function (newData) {
                 console.log("User Added!", newData)
                 currentUser = newData;
 
@@ -72,33 +72,59 @@ function renderClassCards() {
 function createClassCard(classes) {
     for (let i = 0; i < classes.length; i++) {
         const userClassCard =
-            `<div class="col-lg-4 card">
+            `<div class="col-lg-4 char-card">
                 <div>
-                    <div class="card-header" id="${classes[i].id}">
-                        ${classes[i].role}
-                    </div>
                     <div class="card-body">
-                        <img id="userclass-image" src="${classes[i].image}" alt="Product Image">
+                        <img class="select-user-${classes[i].id} char-image-select"id="char-image" data-id="${classes[i].id} "src="${classes[i].image}" alt="Product Image">
                     </div>
+                    <br>
+                    <h4 class="card-header" id="${classes[i].id}">
+                    ${classes[i].role}
+                    </h4>
                     <div class="form-group">
                         <form class="form-row">
-                            <p class="hp" data-id="${classes[i].hp}">Health: ${classes[i].hp}</p>
-                            <p class="ap" data-id="${classes[i].ap}">Attack: ${classes[i].ap}</p>
-                            <p class="dp" data-id="${classes[i].dp}">Defense: ${classes[i].dp}</p>
-                            <p class="description" data-id="${classes[i].description}">${classes[i].description}</p>
-                            <button type="button" class="btn btn-warning btn-default select-user-${classes[i].id}">Select Character!</button>
+                        <div class="stats-div">
+                            <p class="hp char-stats" data-id="${classes[i].hp}">Health: ${classes[i].hp}</p>
+                            <p class="ap char-stats" data-id="${classes[i].ap}">Attack: ${classes[i].ap}</p>
+                            <p class="dp char-stats" data-id="${classes[i].dp}">Defense: ${classes[i].dp}</p>
+                        </div>  
+                            <p class="description char-description" data-id="${classes[i].description}">${classes[i].description}</p>
+                          
                         </form>
                     </div>
                 </div>
             </div>`
         $(".role-cards").append(userClassCard)
-        $(`.select-user-${classes[i].id}`).on("click", function(e) {
+        $(`.select-user-${classes[i].id}`).on("click", function (e) {
             e.preventDefault()
             console.log('character selected', classes[i].id)
             startGame(classes[i].id)
         })
+
+
+        
+        var charImage = document.querySelector('.char-image-select');
+
+        function animateButton(scale, duration, elasticity) {
+            anime.remove(charImage);
+            anime({
+                targets: charImage,
+                scale: scale,
+                duration: duration,
+                elasticity: elasticity
+            });
+        }
+
+        charImage.addEventListener('mouseenter', enterButton, false);
+        charImage.addEventListener('mouseleave', leaveButton, false);
+        function enterButton() { animateButton(1.2, 800, 400) };
+        function leaveButton() { animateButton(1.0, 600, 300) };
+
+
     }
+
 }
+
 
 function startGame(classId) {
     console.log('starting game with user:', currentUser)
@@ -107,7 +133,7 @@ function startGame(classId) {
         url: "/api/currentUser",
         data: {
             userId: currentUser.id,
-            classId, 
+            classId,
         }
     }).then(function (data) {
         if (data.status) {
@@ -119,9 +145,17 @@ function startGame(classId) {
     })
 }
 
+// login animation test 
+
+
+
+
+
+
+
 renderLogIn()
 
-$(".login-submit").on("click", function() {
+$(".login-submit").on("click", function () {
     console.log("click char select")
     tryLogInUser()
 })
